2d74e305d18e76bd9a48cb89042110cab2138e1e
Matias Szlajen <mszlajen@est.frba.utn.edu.ar>
2019-04-23 00:32:23 -0300

    Create to complex

---
+++ src/ToComplex.py
@@ -0,0 +1,45 @@
+from src.ComplexNumber import ComplexNumber
+
+
+class InvalidSintaxError(Exception):
+    pass
+
+
+def to_complex(text):
+    parsing = text.replace(" ", "")
+
+    binomial = False
+    if parsing[0] == "(" and parsing.endswith(")"):
+        binomial = True
+        parsing = parsing.replace("(", "", 1).replace(")", "", 1)
+    elif parsing[0] == "[" and parsing.endswith("]"):
+        parsing = parsing.replace("[", "", 1).replace("]", "", 1)
+    else:
+        raise InvalidSintaxError()
+
+    elements = parsing.split(",")
+
+    if len(elements) != 2:
+        raise InvalidSintaxError()
+
+    if binomial:
+        try:
+            cmp = ComplexNumber.binomial(float(elements[0]), float(elements[1]))
+        except ValueError:
+            raise InvalidSintaxError
+    elif elements[1].endswith("pi"):
+        if elements[1].count("pi") > 1:
+            raise InvalidSintaxError
+        pi_mult = elements[1].replace("pi", "")
+        try:
+            cmp = ComplexNumber.polar_with_pi(float(elements[0]), float(pi_mult))
+        except ValueError:
+            raise InvalidSintaxError
+    else:
+        try:
+            cmp = ComplexNumber.polar_with_decimal(float(elements[0]), float(elements[1]))
+        except ValueError:
+            raise InvalidSintaxError
+
+    return cmp
+

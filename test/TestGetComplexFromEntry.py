aa2dee74427c82f1d16737c898e0d71ff041d952
Matias Szlajen <mszlajen@est.frba.utn.edu.ar>
2019-04-22 23:20:22 -0300

    Create tests for GetComplexFromEntry

---
+++ test/TestGetComplexFromEntry.py
@@ -0,0 +1,37 @@
+import unittest
+from tp_mat_sup import Application
+
+class TestGetComplexFromEntry:
+
+    def test_correct_binomial_of_ints(self):
+        self.set_complex("(1,2)")
+        self.assertEqual(self.complex.real(), 1)
+        self.assertEqual(self.complex.imag(), 2)
+
+    def test_correct_binomial_of_floats(self):
+        self.set_complex("(1.45,2.95)")
+        self.assertEqual(self.complex.real(), 1.45)
+        self.assertEqual(self.complex.imag(), 2.95)
+
+    def test_correct_polar_of_ints(self):
+        self.set_complex("[1,1]")
+        self.assertEqual(self.complex.abs(), 1)
+        self.assertEqual(self.complex.phase(), 1)
+
+    def test_correct_polar_of_floats(self):
+        self.set_complex("[1.2343,0.23]")
+        self.assertEqual(self.complex.abs(), 1.2343)
+        self.assertEqual(self.complex.phase(), 0.23)
+
+    def test_correct_pi_of_ints(self):
+        self.set_complex("[1, 1Pi]")
+        self.assertEqual(self.complex.abs(), 1)
+        self.assertEqual(self.complex.phase(), 1)
+
+    def test_correct_pi_of_floats(self):
+        self.set_complex("[4.25, 0.25Pi]")
+        self.assertEqual(self.complex.abs(), 4.25)
+        self.assertEqual(self.complex.phase(), 0.25)
+
+    def set_complex(self, string):
+        self.complex = Application.get_complex_from_entry(string)
